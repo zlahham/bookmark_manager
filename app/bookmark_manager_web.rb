a2595398a68fbb7c95dc7676a0ac16cda8144624
zlahham <zlahham@gmail.com>
2015-08-11 13:22:46 +0100

    Passed 4 tests

--- app/bookmark_manager_web.rb
+++ app/bookmark_manager_web.rb
@@ -8,7 +8,10 @@ class BookMarkManager < Sinatra::Base
   end
 
   post '/links' do
-    Link.create(url: params[:url], title: params[:title])
+    link = Link.new(url: params[:url], title: params[:title])
+    tag = Tag.create(name: params[:tag])
+    link.tags << tag
+    link.save
     redirect to('/links')
   end
 
